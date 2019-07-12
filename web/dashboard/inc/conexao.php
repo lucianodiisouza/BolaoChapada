962129025d9b83bc9d7c779a2c22479a723642b0
Luciano dii Souza <lucianodiisouza@hotmail.com>
2019-07-12 14:02:18 -0300

    Update conexao.php

--- web/dashboard/inc/conexao.php
+++ web/dashboard/inc/conexao.php
@@ -1,8 +1,8 @@
 <?php
-	define('SERVIDOR','sql262.main-hosting.eu');
-	define('USUARIO','u482896347_root');
-	define('SENHA','aggjlr@123');
-	define('DB','u482896347_bolao');
+	define('SERVIDOR','');
+	define('USUARIO','');
+	define('SENHA','');
+	define('DB','');
 	define('CHARSET','utf8');
 
 	$conecta = mysqli_connect(SERVIDOR, USUARIO, SENHA, DB) or die(mysqli_connect_error());
